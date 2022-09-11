406df6400387538b0a57473b69a11232e9e7a348
Tom Judd <tomjudd@Toms-MacBook.localdomain>
2022-09-10 23:49:48 -0500

    fmt

--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"database/sql"
 	_ "github.com/mattn/go-sqlite3"
 	"net/http"
 )
 
 type Game struct {
-	gameId int `json:"game_id"`
-	name string `json:"name"`
+	gameId   int    `json:"game_id"`
+	name     string `json:"name"`
 	location string `json:"location"`
-} 
+}
 
 var DB *sql.DB
 
@@ -28,24 +28,24 @@ func searchPost(c *gin.Context) {
 	var game Game
 	gameName := c.PostForm("game_name")
 	query := "SELECT * FROM game_fts WHERE name MATCH ?;"
-	rows, err  := DB.Query(query, gameName)
-//	rows, err  := DB.QueryRow(query, gameName)
+	rows, err := DB.Query(query, gameName)
+	//	rows, err  := DB.QueryRow(query, gameName)
 	if err != nil {
 		panic(err)
-  }
-	for rows.Next()  {
+	}
+	for rows.Next() {
 		var name string
-   	err  :=  rows.Scan(&game.gameId,&game.name, &game.location)
-		if err !=  nil  {
+		err := rows.Scan(&game.gameId, &game.name, &game.location)
+		if err != nil {
 			panic(err)
 		}
 		fmt.Println(name)
 	}
-//	if err != nil {
-//		panic(err)
-//  }
-	if game.name != ""{
-    c.Redirect(301, fmt.Sprintf("/game/%v", game.gameId))
+	//	if err != nil {
+	//		panic(err)
+	//  }
+	if game.name != "" {
+		c.Redirect(301, fmt.Sprintf("/game/%v", game.gameId))
 	} else {
 		c.JSON(404, gin.H{"msg": "Game Not Found"})
 	}
@@ -56,14 +56,14 @@ func gameGet(c *gin.Context) {
 	gameId := c.Param("gameid")
 	query := "SELECT * FROM game where game_id = ?;"
 	row := DB.QueryRow(query, gameId)
-	err := row.Scan(&game.gameId, &game.name,  &game.location)
+	err := row.Scan(&game.gameId, &game.name, &game.location)
 
 	if err != nil {
 		panic(err)
-  }
+	}
 	fmt.Println(game.location)
 	c.HTML(http.StatusOK, "game.tmpl", gin.H{
-		"game_name": game.name,
+		"game_name":     game.name,
 		"game_location": game.location,
 	})
 }
@@ -76,19 +76,18 @@ func gameNewPost(c *gin.Context) {
 	statement, err := DB.Prepare(query)
 	fmt.Println(statement)
 	defer statement.Close()
-	if err != nil  {
+	if err != nil {
 		panic(err)
 	}
-	_,  err = statement.Exec(name, location)
+	_, err = statement.Exec(name, location)
 	if err != nil {
 		panic(err)
 	}
 }
 
 func gameNewGet(c *gin.Context) {
-	  c.HTML(http.StatusOK, "game_new.tmpl", gin.H{})
+	c.HTML(http.StatusOK, "game_new.tmpl", gin.H{})
 }
-
 
 func main() {
 	var err error
@@ -98,20 +97,20 @@ func main() {
 	}
 	defer DB.Close()
 
-  if  err != nil {
+	if err != nil {
 		panic(err)
 	}
 
 	router := gin.Default()
-  router.LoadHTMLGlob("templates/*.tmpl")
+	router.LoadHTMLGlob("templates/*.tmpl")
 	router.GET("/", indexGet)
 
 	router.GET("/search", searchGet)
 	router.POST("/search", searchPost)
 
 	router.GET("/game/:gameid", gameGet)
-  router.GET("/game/new", gameNewGet)
-  router.POST("/game/new", gameNewPost)
+	router.GET("/game/new", gameNewGet)
+	router.POST("/game/new", gameNewPost)
 
 	router.Run(":80")
 }
