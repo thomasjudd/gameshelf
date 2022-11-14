a43685d1686b417f3173d795bc663c440667b929
Tom Judd <tomjudd@Toms-MacBook.localdomain>
2022-11-13 21:16:33 -0600

    fix db middleware

--- middleware/db.go
+++ middleware/db.go
@@ -8,13 +8,12 @@ import (
 
 
 func SQLiteMiddleware() gin.HandlerFunc {
-	db, err := sql.Open("sqlite3", "./sqlitedb/game_collection.db")
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
 	return func(c *gin.Context) {
+		db, err := sql.Open("sqlite3", "./sqlitedb/game_collection.db")
+		if err != nil {
+			panic(err)
+		}
+		defer db.Close()
 		c.Set("DBClient",  db)
 		c.Next()
 	}
